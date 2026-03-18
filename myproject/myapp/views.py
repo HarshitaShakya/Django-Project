62f37222a7748f864a4b86c0f3f1b7c4a666cc4f
HarshitaShakya <shakyaharshita20@gmail.com>
2026-03-19 00:18:55 +0530

    update file

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from .models import Member
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    # Fetch all members so the template can render them in a table.
+    members = Member.objects.all()
+
+    return render(request, 'home.html', {
+        'all': members,
+    })
