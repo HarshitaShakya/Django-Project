603f94d456df720dc70dbd24ae949eaef4f3efca
HarshitaShakya <shakyaharshita20@gmail.com>
2026-04-04 23:37:35 +0530

    Add views

---
+++ blog_project/blog_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Post
+from .serializers import PostSerializer
+from .permission import IsAuthorOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+
+
+# Create your views here.
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+
+def perform_create(self, serializer):
+    serializer.save(author=self.request.user)
